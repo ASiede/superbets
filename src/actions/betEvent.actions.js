98dabd2dcc277a8cecb6dc0db256bdcd5ace0dbb
Andrea Siede <andrea.siede@gmail.com>
2020-11-24 20:12:09 -0800

    adds parsing for new betevent submission

---
+++ src/actions/betEvent.actions.js
@@ -0,0 +1,44 @@
+
+export const persistBetEvent = (variables) => async(dispatch) => {
+  const questionKeys = Object.keys(variables).reduce((acc, cur) => {
+    if (cur.includes('q') && !cur.includes(':')) {
+      acc.push(cur);
+    }
+    return acc;
+  }, []);
+  const numberOfQuestions = questionKeys.length;
+  const questions = [];
+  for (let i=0; i < numberOfQuestions; i++) {
+    const answers = Object.keys(variables).reduce((acc, cur) => {
+      if (cur.includes(`q${i}:a`) && !cur.includes('odds')) {
+        console.log('cur', cur);
+        console.log('the', parseInt(cur.split(':')[1].substring(1)) + 1);
+        acc.push({
+          answerId: parseInt(cur.split(':')[1].substring(1)) + 1,
+          text: variables[cur],
+          odds: variables[`${cur}:odds`],
+          confirmed: false
+        });
+      }
+      return acc;
+    }, []);
+    questions.push({
+      questionId: i + 1,
+      text: variables[`q${i}`],
+      answers
+    });
+  }
+  const body = {
+    name: variables.name,
+    password: variables.password,
+    questions: questions
+  };
+  // return body;
+  const response = await fetch(`${baseUrl}`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(body)
+  });
+  const newResponse = await response.json();
+  console.log('POSTING BET EVENT', newResponse);
+};
