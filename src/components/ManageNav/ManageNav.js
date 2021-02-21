b83b72faecf0d1c5c6c48ecad71fb66b0f5bc6c8
Andrea Siede <andrea.siede@gmail.com>
2021-02-21 11:50:54 -0800

    updates manageNave style

--- src/components/ManageNav/ManageNav.js
+++ src/components/ManageNav/ManageNav.js
@@ -3,11 +3,17 @@ import { Link } from 'react-router-dom';
 import './ManageNav.css';
 
 export const ManageNav = () => (
-  <div className='wrapper-style'>
-    <nav className='nav-style'>
-      <Link className='link-style' to='/createNew'>CREATE NEW BET EVENT</Link>
-      <Link className='link-style' to='/confirmAnswers'>CONFIRM ANSWERS</Link>
-      <Link className='link-style' to='/editBetEvent'>EDIT BET EVENT</Link>
+  <div className='manage-nav-container'>
+    <nav className='manage-nav'>
+      <Link className='manage-link' to='/createNew'>
+        CREATE NEW BET EVENT
+      </Link>
+      <Link className='manage-link' to='/confirmAnswers'>
+        CONFIRM ANSWERS
+      </Link>
+      <Link className='manage-link' to='/editBetEvent'>
+        EDIT BET EVENT
+      </Link>
     </nav>
   </div>
 );
