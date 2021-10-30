ba4bebf59bc7e7580915ef7a3b4860cf63c4b906
Andrea Siede <andrea.siede@gmail.com>
2021-10-30 16:15:43 -0700

    shows users events in dropdown

--- src/components/ConfirmAnswers/ConfirmAnswers.js
+++ src/components/ConfirmAnswers/ConfirmAnswers.js
@@ -1,14 +1,44 @@
-import React, { useRef } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
+import React, {
+  // useRef,
+  useEffect,
+  useState
+} from 'react';
+import {
+  useSelector
+  // useDispatch
+} from 'react-redux';
+import { Dropdown } from 'primereact/dropdown';
+import { getEventsByUser } from '../../utils/events/events';
 // import { Messages } from 'primereact/messages';
 
 export const ConfirmAnswers = () => {
   // const dispatch = useDispatch();
-  // const newBetEvent = useSelector((state) => state.betEvents.newBetEvent);
+  const userId = useSelector((state) => state.user.id);
   // const persistingBetEvent = useSelector((state) => state.persistingBetEvent);
+  const [betEvents, setBetEvents] = useState([]);
+  const [selectedEvent, setSelectedEvent] = useState();
+  useEffect(
+    () =>
+      (async () => {
+        const events = await getEventsByUser(userId);
+        setBetEvents(events);
+      })(),
+    []
+  );
+
   return (
     <div>
       <p>CONFIRMING ANSWERS</p>
+      <Dropdown
+        value={selectedEvent}
+        options={betEvents}
+        onChange={(target) => setSelectedEvent(target.value)}
+        optionLabel='name'
+        filter
+        showClear
+        filterBy='name'
+        placeholder='Select a Country'
+      />
     </div>
   );
 };
